a46043a238c3fd7ed0b7348e6be7cef659d4712e
Nate Jensvold <jensvoldnate@gmail.com>
2022-04-11 10:42:29 +0000

    Moved base config of config.py to default_config.py to allow config.py to get added to .gitignore

--- albedo_bot/config/__init__.py
+++ albedo_bot/config/__init__.py
@@ -1,13 +1,17 @@
-from .config import token, database_name, VERBOSE
+
+import os
+import shutil
+cur_dir = os.path.abspath(os.path.dirname(__file__))
+if "config.py" not in os.listdir(cur_dir):
+    shutil.copyfile(os.path.join(cur_dir, "default_config.py"),
+                    os.path.join(cur_dir, "config.py"))
+# autopep8: off
 from .json_config import (
     permissions, database, prefixes, blacklist)
-
-from .json_config import (
-    AFK_HELPER_PATH, BLACKLIST_JSON_PATH, CONFIG_FOLDER_PATH, DATABASE_JSON_PATH,
-    GUILD_JSON_PATH, HERO_ALIAS_JSON_PATH, HERO_JSON_PATH, PERMISSIONS_JSON_PATH,
-    PREFIX_JSON_PATH, AFK_HELPER_IMAGE_PREFIX)
+from .config import token, database_name, VERBOSE # pylint: disable=wrong-import-position
 
 if token == "":
     if token is None or len(token) == 0:
         raise Exception(
-            "Set 'token' Environment variable at before running bot")
+            "Set 'token' Environment variable at "
+            f"({os.path.join(cur_dir, 'config.py')}) before running bot")
